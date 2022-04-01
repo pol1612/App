451cdfa8068480a2ee0469daa91cca7dd4db6192
Pol Sañé Jové <pol.sane@uvic.cat>
2022-04-01 20:14:27 +0200

    Used methods from 'File' to check if it's a file and to know its path

--- src/main/java/io/App.java
+++ src/main/java/io/App.java
@@ -5,6 +5,9 @@ import java.io.File;
 public class App {
     public static void main(String[] args){
         var file1=new File("src/main/resources/file1.txt");
-        System.out.println(""+file1.exists());
+        System.out.println("Exists:"+file1.exists());
+        System.out.println("Full path:"+file1.getAbsolutePath());
+        System.out.println("Is File:"+file1.isFile());
+
     }
 }
